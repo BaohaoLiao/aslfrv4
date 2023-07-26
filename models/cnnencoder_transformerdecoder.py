dc9cd165383957763f20daed8efc2b9ca6c08286
baohao <liaobaohao@gmail.com>
2023-07-26 19:42:49 +0200

    add early stop for inference

--- models/cnnencoder_transformerdecoder.py
+++ models/cnnencoder_transformerdecoder.py
@@ -691,7 +691,7 @@ class TFLiteModelv2(tf.Module):
                 dec_input=dec_input,
                 encoder_out=encoder_out,
                 encoder_attention_mask=encoder_attention_mask)
-            logits = tf.concat([logits[:, :, :59], logits[:, :, 61:]], axis=-1)
+            #logits = tf.concat([logits[:, :, :59], logits[:, :, 61:]], axis=-1)
             logits = tf.argmax(logits, axis=-1, output_type=tf.int32)
             last_logit = logits[:, -1][..., tf.newaxis]
             dec_input = tf.concat([dec_input, last_logit], axis=-1)

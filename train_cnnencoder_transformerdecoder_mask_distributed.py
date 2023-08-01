5f775678511921c0098b805c35c50b2b41c7d353
baohao <liaobaohao@gmail.com>
2023-08-01 11:55:01 +0200

    add conformer droppath

--- train_cnnencoder_transformerdecoder_mask_distributed.py
+++ train_cnnencoder_transformerdecoder_mask_distributed.py
@@ -12,6 +12,7 @@ from optimizer import LRInverseSqrtScheduler
 from display import DisplayOutputs, DisplayOutputsv2
 from mask.cnnencoder_transformerdecoder_mask import CNNEncoderTransformerDecoder
 from mask.conformerencoder_transformerdecoder_mask import ConformerEncoderTransformerDecoder
+from mask.conformerencoder_transformerdecoder_mask_droppath import ConformerEncoderTransformerDecoder as ConformerEncoderTransformerDecoderDroppath
 from metadata import XY_POINT_LANDMARKS
 
 
@@ -33,7 +34,8 @@ def parse_args():
     # Model args
     parser.add_argument("--model_arch",
                         choices=["transformer", "cnnencoder_transformerdecoder", "cnnencoderbn_transformerdecoder",
-                                 "cnnencoderv2_transformerdecoder", "conformerencoder_transformerdecoder"],
+                                 "cnnencoderv2_transformerdecoder", "conformerencoder_transformerdecoder",
+                                 "conformerencoder_transformerdecoder_droppath"],
                         default="transformer")
     parser.add_argument("--num_encoder_layers", type=int, default=3)
     parser.add_argument("--encoder_hidden_dim", type=int, default=384)
@@ -192,6 +194,29 @@ def main():
                 activation=args.activation)
         elif args.model_arch == "conformerencoder_transformerdecoder":
             model = ConformerEncoderTransformerDecoder(
+                num_encoder_layers=args.num_encoder_layers,
+                encoder_hidden_dim=args.encoder_hidden_dim,
+                encoder_mlp_dim=args.encoder_mlp_dim,
+                encoder_num_heads=args.encoder_num_heads,
+                encoder_conv_dim=args.encoder_conv_dim,
+                encoder_kernel_size=args.encoder_kernel_size,
+                encoder_dilation_rate=args.encoder_dilation_rate,
+                max_source_length=args.max_source_length,
+                num_decoder_layers=args.num_decoder_layers,
+                vocab_size=args.vocab_size,
+                decoder_hidden_dim=args.decoder_hidden_dim,
+                decoder_mlp_dim=args.decoder_mlp_dim,
+                decoder_num_heads=args.decoder_num_heads,
+                max_target_length=args.max_target_length,
+                pad_token_id=args.pad_token_id,
+                emb_dropout=args.emb_dropout,
+                attn_dropout=args.attn_dropout,
+                hidden_dropout=args.hidden_dropout,
+                learnable_position=args.learnable_position,
+                prenorm=args.prenorm,
+                activation=args.activation)
+        elif args.model_arch == "conformerencoder_transformerdecoder_droppath":
+            model = ConformerEncoderTransformerDecoderDroppath(
                 num_encoder_layers=args.num_encoder_layers,
                 encoder_hidden_dim=args.encoder_hidden_dim,
                 encoder_mlp_dim=args.encoder_mlp_dim,

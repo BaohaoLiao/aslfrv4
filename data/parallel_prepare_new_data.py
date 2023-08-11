4790f66d65edbe8e889ec517a28bbc13665ac817
baohao <liaobaohao@gmail.com>
2023-08-11 20:43:24 +0200

    add ctc awp

--- data/parallel_prepare_new_data.py
+++ data/parallel_prepare_new_data.py
@@ -32,7 +32,7 @@ mp_holistic = mp.solutions.holistic
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*3) * np.nan
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3) * np.nan
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(478*3) * np.nan
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3) * np.nan
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3) * np.nan
     return np.concatenate([face, pose, lh, rh])

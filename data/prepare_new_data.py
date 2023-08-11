6a4929a8be3550dc427a6305ce3dbfbae4610841
baohao <liaobaohao@gmail.com>
2023-08-11 15:08:00 +0200

    add ctc awp

---
+++ data/prepare_new_data.py
@@ -0,0 +1,70 @@
+import os
+import fire
+import numpy as np
+import pandas as pd
+import cv2
+import mediapipe as mp
+from metadata import XY_POINT_LANDMARKS
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*3) * np.nan
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3) * np.nan
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3) * np.nan
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3) * np.nan
+    return np.concatenate([face, pose, lh, rh])
+
+def main(data_dir: str, output_path: str):
+    df = pd.read_csv(os.path.join(data_dir, "batch.csv"))
+
+    with mp_holistic.Holistic(static_image_mode=True, model_complexity=2, refine_face_landmarks=True) as holistic:
+        landmarks = []
+        frames = []
+        file_names = []
+        for index, row in df.iterrows():
+            if index > 10:
+                break
+            image_files = []
+            for i in range(row["number_of_frames"]):
+                image_files.append(os.path.join(data_dir, row["filename"]) + "/" + str(i+1).zfill(4) + ".jpg")
+                frames.append(i)
+                file_names.append(row["filename"])
+            for idx, file in enumerate(image_files):
+                image = cv2.imread(file)
+                image_height, image_width, _ = image.shape
+                results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+                landmarks.append(extract_keypoints(results))
+
+    landmarks = np.vstack(landmarks)
+    assert len(frames) == len(file_names) == np.shape(landmarks)[0]
+
+    parquet_df = {}
+    parquet_df["file_name"] = file_names
+    parquet_df["frame"] = frames
+
+    for i in range(478):
+        for j, coord in enumerate(["x", "y", "z"]):
+            parquet_df[coord + "_face_" + str(i)] = landmarks[:, i * 3 + j]
+    for i in range(33):
+        for j, coord in enumerate(["x", "y", "z"]):
+            parquet_df[coord + "_pose_" + str(i)] = landmarks[:, 478 * 3 + i * 3 + j]
+    for i in range(21):
+        for j, coord in enumerate(["x", "y", "z"]):
+            parquet_df[coord + "_left_hand_" + str(i)] = landmarks[:, 478 * 3 + 33 * 3 + i * 3 + j]
+    for i in range(21):
+        for j, coord in enumerate(["x", "y", "z"]):
+            parquet_df[coord + "_right_hand_" + str(i)] = landmarks[:, 478 * 3 + 33 * 3 + 21 * 3 + i * 3 + j]
+
+    parquet_df = pd.DataFrame(parquet_df)
+    selected_columns = ["file_name"] + ["frame"] + XY_POINT_LANDMARKS
+    filtered_parquet_df = parquet_df[selected_columns]
+    filtered_parquet_df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)

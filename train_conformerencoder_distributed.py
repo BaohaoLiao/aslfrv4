dec829e3b526ab00bd084f68f1d722f0083a0123
baohao <liaobaohao@gmail.com>
2023-08-12 13:28:02 +0200

    add conformerencoder.py

--- train_conformerencoder_distributed.py
+++ train_conformerencoder_distributed.py
@@ -121,7 +121,7 @@ def main():
         char_to_num=char_to_num,
         augment=True,
         repeat=True,
-        shuffle=30000,
+        shuffle=3000,
         drop_remainder=True
     )
     val_dataset = load_dataset(

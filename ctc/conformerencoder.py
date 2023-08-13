9cefa5505dd470ce295cc7e9416228272ef66dd9
baohao <liaobaohao@gmail.com>
2023-08-13 15:35:50 +0200

    add conformerencoder.py

--- ctc/conformerencoder.py
+++ ctc/conformerencoder.py
@@ -409,9 +409,8 @@ class TFLiteModelBestPath(tf.Module):
         length = tf.reduce_sum(tf.cast(logits_mask, tf.int32), axis=-1)
         encoder_out = self.model.encoder(x, mask=logits_mask, training=False)
         logits = self.model.ctc_head(encoder_out)
-        #pred = tf.argmax(logits, axis=-1, output_type=tf.int32)
-        #return pred, length
-        return logits, length
+        pred = tf.argmax(logits, axis=-1, output_type=tf.int32)
+        return pred, length
 
     @tf.function(input_signature=[tf.TensorSpec(shape=[None, len(XY_POINT_LANDMARKS)], dtype=tf.float32, name='inputs')])
     def __call__(self, inputs, training=False):
@@ -425,12 +424,7 @@ class TFLiteModelBestPath(tf.Module):
         x, length = self.encoder(x)
         x = x[0]
 
-        length = [len(x)]
-        x = tf.expand_dims(x, axis=1)
-        x = tf.nn.ctc_greedy_decoder(x, length, merge_repeated=True, blank_index=self.pad_token_id)[0][0].values
-
-        """        
-        shifted_x = tf.roll(x, shift=-1, axis=0)
+        shifted_x = tf.concat([x[1:], x[:1]], axis=0)
         is_same_as_next = tf.math.equal(x[:-1], shifted_x[:-1])
 
         # Add a 'False' to the end to keep the last element
@@ -439,7 +433,6 @@ class TFLiteModelBestPath(tf.Module):
         # Filter out elements that are duplicates or equal to pad_token_id
         x_deduplicated = tf.boolean_mask(x, tf.math.logical_not(is_same_as_next))
         x = tf.boolean_mask(x_deduplicated, tf.math.not_equal(x_deduplicated, self.pad_token_id))
-        """
 
         x = x[:self.max_gen_length]
         x = tf.one_hot(x, 59) # how about not in 59?

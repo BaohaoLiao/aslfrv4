f9c075cba81c21371c52dc57d25ceb7621c32c78
baohao <liaobaohao@gmail.com>
2023-08-14 14:41:11 +0200

    add conformerencoder.py

--- auto_ctc/conformerencoder_transformerdecoder_mask_droppath_ctc.py
+++ auto_ctc/conformerencoder_transformerdecoder_mask_droppath_ctc.py
@@ -827,7 +827,7 @@ class TFLiteModelEnsembleAutoCTC(tf.Module):
                 last_logit = logits[:, -1][..., tf.newaxis]
             tf.print(last_logit[0] == self.end_token_id, tf.logical_or(stop, last_logit[0] == self.end_token_id))
             dec_input = tf.concat([dec_input, last_logit], axis=-1)
-            #stop = tf.logical_or(stop, last_logit[0] == self.end_token_id)
+            stop = tf.logical_or(stop, last_logit[0] == self.end_token_id)
 
         x = dec_input[0]
         idx = tf.argmax(tf.cast(tf.equal(x, self.end_token_id), tf.int32))
